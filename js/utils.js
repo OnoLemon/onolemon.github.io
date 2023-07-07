e598a11668381fc7094975984b4860d3d666b1c1
OnoLemon <3033825226@qq.com>
2023-07-08 00:03:22 +0800

    Site updated: 2023-07-08 00:03:22

--- js/utils.js
+++ js/utils.js
@@ -550,7 +550,7 @@ const anzhiyu = {
         t
       );
     };
-    $sayhiEl && ($sayhiEl.innerHTML = getTimeState() + "！我是");
+    $sayhiEl && ($sayhiEl.innerHTML = getTimeState() + "！我是小野");
   },
   // 友链注入预设评论
   addFriendLink() {
